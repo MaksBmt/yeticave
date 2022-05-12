4880130857215dae2a7cefef8b1a3820a5bae3a6
MaksBmt <solov@tdpsm.ru>
2022-05-12 21:13:14 +0300

    вносит исправления (исправляет переменную с категорией в карточку товара) и организует немного по другому код. Как в видео разборе

--- templates/main.php
+++ templates/main.php
@@ -19,13 +19,13 @@
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
         <?php foreach($lots as $lot): ?>
-           
+          
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="<?= $lot["image"]; ?>" width="350" height="260" alt="<?= $lot["title"]; ?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $lot["category"]; ?></span>
+                    <span class="lot__category"><?= $lot["name_category"]; ?></span>
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($lot["title"]); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
